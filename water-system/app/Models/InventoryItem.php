534fb4daa376eb6076ef9cc6398255825b104f38
Blacops111 <dilenfortune@gmail.com>
2026-09-15 00:03:25 +0300

    Connect inventory items to production recipes

--- water-system/app/Models/InventoryItem.php
+++ water-system/app/Models/InventoryItem.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class InventoryItem extends Model
 {
@@ -34,6 +35,16 @@ class InventoryItem extends Model
     public function waterMeters(): HasMany
     {
         return $this->hasMany(WaterMeter::class);
+    }
+
+    public function productionRecipe(): HasOne
+    {
+        return $this->hasOne(ProductionRecipe::class, 'finished_product_id');
+    }
+
+    public function recipeComponents(): HasMany
+    {
+        return $this->hasMany(ProductionRecipeComponent::class);
     }
 
     public function stockBalance(): float
