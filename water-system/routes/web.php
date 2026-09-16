00ae91b31fc4d661c620386538b95ec6ba4ce83c
Blacops111 <dilenfortune@gmail.com>
2026-09-16 11:20:22 +0300

    Add V2 sale completion route

--- water-system/routes/web.php
+++ water-system/routes/web.php
@@ -110,6 +110,9 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::post('/sales-orders/{salesOrder}/items', [SalesOrderController::class, 'storeItem'])
         ->name('sales-orders.items.store');
 
+    Route::post('/sales-orders/{salesOrder}/complete', [SalesOrderController::class, 'complete'])
+        ->name('sales-orders.complete');
+
     Route::get('/products', [ProductController::class, 'index'])
         ->name('products.index');
 
