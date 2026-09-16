fcb4861cde385be0abaf48b6c5d9d617c6795290
Blacops111 <dilenfortune@gmail.com>
2026-09-16 12:20:34 +0300

    Add V2 sales report Excel route

--- water-system/routes/web.php
+++ water-system/routes/web.php
@@ -107,6 +107,9 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/sales-orders/report/pdf', [SalesOrderController::class, 'reportPdf'])
         ->name('sales-orders.report.pdf');
 
+    Route::get('/sales-orders/report/excel', [SalesOrderController::class, 'reportExcel'])
+        ->name('sales-orders.report.excel');
+
     Route::get('/sales-orders/create', [SalesOrderController::class, 'create'])
         ->name('sales-orders.create');
 
