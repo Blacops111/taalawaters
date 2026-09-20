a34f250fee747bcdb58581189bc57af8918aad3e
Blacops111 <dilenfortune@gmail.com>
2026-09-20 21:05:47 +0300

    Add drivers to application navigation

--- water-system/resources/views/layouts/app.blade.php
+++ water-system/resources/views/layouts/app.blade.php
@@ -127,6 +127,7 @@
                     <a href="{{ route('sales-orders.create') }}" class="taala-nav-link">Sales</a>
                     <a href="{{ route('purchase-requests.index') }}" class="taala-nav-link">Purchasing</a>
                     <a href="{{ route('suppliers.index') }}" class="taala-nav-link">Suppliers</a>
+                    <a href="{{ route('drivers.index') }}" class="taala-nav-link">Drivers</a>
                 </div>
 
                 @auth
