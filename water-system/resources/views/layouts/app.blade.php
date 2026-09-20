d755fdd75d72e1a573a3d29e9a61e320ecc96316
Blacops111 <dilenfortune@gmail.com>
2026-09-20 21:51:33 +0300

    Add assignments to logistics navigation

--- water-system/resources/views/layouts/app.blade.php
+++ water-system/resources/views/layouts/app.blade.php
@@ -129,6 +129,7 @@
                     <a href="{{ route('suppliers.index') }}" class="taala-nav-link">Suppliers</a>
                     <a href="{{ route('drivers.index') }}" class="taala-nav-link">Drivers</a>
                     <a href="{{ route('vehicles.index') }}" class="taala-nav-link">Vehicles</a>
+                    <a href="{{ route('vehicle-assignments.index') }}" class="taala-nav-link">Assignments</a>
                 </div>
 
                 @auth
