f051e533f1806c9a2e90304a962b33d50b33750c
Blacops111 <dilenfortune@gmail.com>
2026-09-20 23:29:34 +0300

    Expose driver profile on user

--- water-system/app/Models/User.php
+++ water-system/app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Authenticatable
 {
@@ -41,6 +42,11 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+    public function driverProfile(): HasOne
+    {
+        return $this->hasOne(Driver::class);
+    }
+
     protected function casts(): array
     {
         return [
