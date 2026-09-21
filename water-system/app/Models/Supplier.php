7666d7ea28162975608f06cfaa5510883ded2a37
Blacops111 <dilenfortune@gmail.com>
2026-09-21 13:06:55 +0300

    Link suppliers to payments

--- water-system/app/Models/Supplier.php
+++ water-system/app/Models/Supplier.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Supplier extends Model
 {
@@ -20,5 +21,10 @@ class Supplier extends Model
         return [
             'is_active' => 'boolean',
         ];
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(SupplierPayment::class);
     }
 }
